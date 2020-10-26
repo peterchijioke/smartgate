909fd825ea4872d1a7d02d2d6b7e7c79a04b6d68
peter2c <peter2c@yahoo.com>
2020-10-26 11:50:45 +0100

    little update.

--- controlers/UserControllers.js
+++ controlers/UserControllers.js
@@ -3,16 +3,18 @@ const controler = {};
 const UsersModel = require("../models/UserModel");
 
 controler.create = async (req, res) => {
-  const data = await UsersModel.create({
-    name: "Paul Jonah",
-    pin: "",
-  })
-    .then(function (data) {
-      return data;
-    })
-    .catch((error) => {
-      return error;
-    });
+  // const data = await UsersModel.create({
+  //   name: "Paul Jonah",
+  //   pin: "",
+  // })
+  //   .then(function (data) {
+  //     return data;
+  //   })
+  //   .catch((error) => {
+  //     return error;
+  //   });
+
+  const data = { name: "peter chijioke", age: 28 };
 
   res.json(data);
 };
