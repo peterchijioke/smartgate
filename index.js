3637eb7147bd6bcc3b1134813e4f9080601d4ff1
peter2c <peter2c@yahoo.com>
2020-10-26 10:14:53 +0100

    push

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Allow", "POST", "GET", "PUT", "OPTIONS", "DELETE");
+  res.header(
+    "Access-control-Allow-Headers",
+    "POST",
+    "GET",
+    "PUT",
+    "OPTIONS",
+    "DELETE"
+  );
+  next();
+});
+
+// initialize router
+
+app.use("/newUser", require("./routes/userRoute"));
+
+// initialze
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port`);
+});
